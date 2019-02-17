d3c81f9feaf61ee1f60831b1fb9df5cd4f649b24
LeTranAnhVu <vule5726@gmail.com>
2019-02-17 17:45:34 +0700

    doing undo redo feature

--- main.js
+++ main.js
@@ -59,15 +59,15 @@ Vue.component('Todo', {
         </div>
     `,
     computed: {
-        isShow: function() {
+        isShow: function () {
             const type = this.filtertype;
-            if(type === 'all'){
+            if (type === 'all') {
                 isShow = true;
-            } else if (type === 'doned' && this.todo.isDone){
+            } else if (type === 'doned' && this.todo.isDone) {
                 isShow = true;
-            } else if (type === 'active' && !this.todo.isDone){
+            } else if (type === 'active' && !this.todo.isDone) {
                 isShow = true;
-            }else {
+            } else {
                 isShow = false;
             }
             return isShow;
@@ -87,34 +87,70 @@ const app = new Vue({
         todos: Array(...seedTodos),
         newTodo: null,
         filtertype: 'all',
+        tasks: [],
+        redoTask: [],
     },
 
 
     // methods
     methods: {
         // ADD
-        addTodo: function () {
+        addTodo: function (rawNewTodo, index) {
             // verify the new todo
-            const newTodo = this.newTodo.trim();
-            if (newTodo) {
-                // add new todo
-                let fakeId = this.todos.length + 200;
-                this.todos.push({
-                    id: fakeId,
-                    content: newTodo,
-                    isDone: false
-                });
+            if (typeof rawNewTodo == 'string') {
+
+                const newTodo = rawNewTodo.trim();
+                console.log('addTodo', rawNewTodo);
+                // let newTodo = 'tested';
+                if (newTodo) {
+                    // add new todo
+                    let fakeId = this.todos.length + 200;
+                    this.todos.push({
+                        id: fakeId,
+                        content: newTodo,
+                        isDone: false
+                    });
+                }
+                // clear the input
+                this.newTodo = '';
+
+            } else if (typeof rawNewTodo == 'object' && Number.isInteger(index)) {
+
+                // insert the rawNewTodo into the given position
+                this.todos.splice(index, 0, rawNewTodo);
+
+            } else {
+
+                throw ('error in addTodo');
             }
-            // clear the input
-            this.newTodo = '';
         },
         // DELETE
         deleteTodo: function ($event) {
             console.log('call deleted');
 
             let deletedId = $event;
-            this.todos = this.todos.filter(todo => {
-                return todo.id !== deletedId;
+            this.todos = this.todos.filter((todo, index) => {
+                if (todo.id === deletedId) {
+                    // this is the todo need to delete
+                    // and not return this todo
+                    // TODO: 
+                    // add the action into undoTask
+                    let undoTask = {
+                        action: this.addTodo.bind(this),
+                        params: [todo, index],
+                    }
+                    let currentTask = {
+                        action: this.deleteTodo.bind(this),
+                        params: [$event]
+                    }
+                    // TODO
+                    // this.tasks.push(undoTask);
+                    // this.tasks.push(currentTask);
+
+                } else {
+                    // for those todo which not match we return them
+                    return true;
+                }
             });
         },
 
@@ -125,21 +161,39 @@ const app = new Vue({
             });
         },
         // change status
-        toggleTodo: function(updatedId) {
+        toggleTodo: function (updatedId) {
             this.todos = this.todos.map(todo => {
-                if(todo.id === updatedId){
+                if (todo.id === updatedId) {
                     todo.isDone = !todo.isDone;
                 }
                 return todo;
             });
         },
-       
+
+
+        // undo process
+        undo: function() {
+            // check the undotask
+            if(this.tasks && this.tasks.length !== 0){
+                // go to previous task
+                const task = this.undoTask.pop();
+                if(task && typeof task === 'object'){
+                    // excecute the last action
+                    task.action.apply(this, task.params);
+                }
+            }
+        },
+
+        redo_deleteTodo(todo, index) {
+            addTodo(todo, index);
+        },
+
     },
 
 
     // computed
     computed: {
-        doneTodos: function(){
+        doneTodos: function () {
             /*
                 collection of done todos
             */
@@ -147,7 +201,7 @@ const app = new Vue({
                 return todo.isDone === true;
             })
         },
-        activeTodos: function(){
+        activeTodos: function () {
             /*
                 collection of undone todos
             */
@@ -155,8 +209,19 @@ const app = new Vue({
                 return todo.isDone === false;
             })
         },
-        showDeleteAllBtn: function(){
+        showDeleteAllBtn: function () {
             return (this.doneTodos.length !== 0) ? true : false;
+        },
+        currentTaskIndex: function () {
+            // observer the tasks length
+            return this.tasks.length -1;
+        }
+    },
+
+    // watch
+    watch: {
+        undoTask: function(){
+            console.log('undoTask updated', this.undoTask);
         }
     }
 });
